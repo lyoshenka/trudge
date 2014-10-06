c309b673f93341fd5769ec06fbf08a4ef07ebe22
Alex Grintsvayg <lyoshenka@gmail.com>
2014-10-06 10:10:54 -0400

    trudge login

---
+++ site/panel/accounts/trudge.php
@@ -0,0 +1,11 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# setup your account credentials here
+# it's highly recommended to use md5 or sha1 encryption 
+# for your passwords. read more about encryption in the
+# docs: http://getkirby.com/docs/panel/accounts
+
+username: trudge
+password: $2y$10$XSCT95ARqeLHQMmMXEvmUerB6j55VlLdZYGN/9Oo/RasdBEgmwWJC
+encrypt: bcrypt
+language: en

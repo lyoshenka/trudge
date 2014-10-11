c2ed21fbad672dd7a8ebdfefbc5d8464a8dc6db3
Alex Grintsvayg <lyoshenka@gmail.com>
2014-10-11 13:10:20 -0400

    more

--- site/accounts/grin.php
+++ site/accounts/grin.php
@@ -8,10 +8,10 @@ language: en
 role: admin
 token: e2d04c5193d9f903f6a8bfd8b5d6d4e9ce937cce
 history:
+  - fgiving/green-jacket-winners
   - fgiving/2006
-  - fgiving/2008
-  - fgiving/2007
+  - fgiving/2011
   - fgiving/2013
-  - fgiving/2012
+  - fgiving/2010
 firstname: Grin
 lastname:

a34237cbf42b19ee37193939abc6d1545d470ed1
Alex Grintsvayg <lyoshenka@gmail.com>
2015-11-17 14:18:48 -0500

    frolf maps, records

--- site/accounts/grin.php
+++ site/accounts/grin.php
@@ -8,10 +8,10 @@ language: en
 role: admin
 token: 7db19aa6a3f71bb29c7855c41f7416c9819e34d4
 history:
+  - frolf/course-maps
+  - frolf/how-to-play
   - results/season-results
   - frolf
   - results
-  - fgiving/green-jacket-winners
-  - frolf/how-to-play
 firstname: Grin
 lastname:

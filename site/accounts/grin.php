dcb06082676d6d9f9043132f3fd30cf6bbcea1d6
Alex Grintsvayg <lyoshenka@gmail.com>
2017-11-18 15:33:00 -0500

    fgiving 2017

--- site/accounts/grin.php
+++ site/accounts/grin.php
@@ -6,12 +6,12 @@ password: >
   $2a$10$WyPi5lhdjoAcLr0kQQm9U.hibvnJfvMK4cMkMSxyttkZBcw/eulAG
 language: en
 role: admin
-token: fafd6f410010a791c915039cdc078387f45798a0
+token: 645ce185d7cf6eb1e93bab7ba6c3fddc99406861
 history:
+  - fgiving/2017
   - fgiving/2016
   - frolf/green-jacket-winners
   - frolf/course-records
   - frolf/how-to-play
-  - fgiving/2015
 firstname: Grin
 lastname:
